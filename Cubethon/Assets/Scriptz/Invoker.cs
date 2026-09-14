43af226cac7a27b7b466f105252de52483e2a661
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-09-13 19:20:38 -0700

    Update Invoker.cs

--- Cubethon/Assets/Scriptz/Invoker.cs
+++ Cubethon/Assets/Scriptz/Invoker.cs
@@ -7,15 +7,63 @@ public class Invoker : MonoBehaviour
     private bool _isRecording;
     private bool _isReplaying;
     private float _replayTime;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private float _recordingTime;
+    private SortedList<float, Command> _recordedCommands = new SortedList<float, Command>();
+
+    public void ExecuteCommand(Command command)
     {
-        
+        if(_isRecording)
+        {
+            _recordedCommands.Add(_recordingTime, command);
+            Debug.Log("Recorded Time: " + _recordingTime);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Record()
     {
-        
+        _recordingTime = 0.0f;
+        _isRecording = true;
+    }
+
+    public void Replay()
+    {
+        _replayTime = 0.0f;
+        _isReplaying = true;
+
+        if(_recordedCommands.Count > 0)
+        {
+            Debug.LogError("No commands to replay!");
+        }
+        _recordedCommands.Reverse();
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(_isRecording)
+        {
+            _recordingTime += Time.deltaTime;
+        }
+
+        if(_isReplaying)
+        {
+            _replayTime += Time.deltaTime;
+
+            if(_recordedCommands.Any())
+            {
+                if(Mathf.Approximately(_replayTime, _recordedCommands.Keys[0]))
+                {
+                    Debug.Log("Replay Time: " + _replayTime);
+                    Debug.Log("Replay Command" + _recordedCommands.Values[0]);
+
+                    _recordedCommands.Values[0].Execute();
+                    _recordedCommands.RemoveAt(0);
+                }
+            }
+            else
+            {
+                _isReplaying = false;
+            }
+        }
     }
 }
