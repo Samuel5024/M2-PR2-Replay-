e80eb5b7fb2b867bd49f12c772f501dba4418ad3
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-09-14 09:06:10 -0700

    Update InputHandler.cs

--- Cubethon/Assets/Scriptz/InputHandler.cs
+++ Cubethon/Assets/Scriptz/InputHandler.cs
@@ -20,11 +20,11 @@ public class InputHandler : MonoBehaviour
     {
         if(!_isReplaying && _isRecording)
         {
-            if(Input.GetKeyUp(KeyCode.LeftArrow))
+            if(Input.GetKey(KeyCode.LeftArrow))
             {
                 _invoker.ExecuteCommand(_buttonLeft);
             }
-            if(Input.GetKeyUp(KeyCode.RightArrow))
+            if(Input.GetKey(KeyCode.RightArrow))
             {
                 _invoker.ExecuteCommand(_buttonRight);
             }
