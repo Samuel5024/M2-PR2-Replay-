14541c68a009b1959727b1869111aec03cc860c6
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-09-13 17:04:05 -0700

    Add Invoker.cs

---
+++ Cubethon/Assets/Scriptz/Invoker.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Invoker : MonoBehaviour
+{
+    private bool _isRecording;
+    private bool _isReplaying;
+    private float _replayTime;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
